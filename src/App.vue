840204cb4e1f02e4f5c5608c70ecef47a7a3f8c3
unknown <cc60614cc60614@gmail.com>
2020-11-19 17:14:49 +0800

    day3-2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <div>
-    <router-link to='/todo-list'>Todo List</router-link>
-    <router-link to='/counter'>Counter</router-link>
-    <router-link to ='/area-select'>area-select</router-link>
-    <router-link to ='/currency'>currency</router-link>
-    <router-link to ='/form'>form</router-link>
-    <router-link to ='/style'>style</router-link>
-    <router-link to ='/toggle'>toggle</router-link>
-    <router-link to ='/users'>users</router-link>
-    <router-link to ='/policy'>policy</router-link>
-    {{ count }}
-    <hr/>
-     <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <v-toolbar-title> Vue Lesson </v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.path" :to="item.path">
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.name }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 <script>
-  export default {
-
-  };
+export default {
+  data: () => ({
+    drawer: false,
+  }),
+  computed: {
+    items() {
+      return [
+        { path: "/todo-list", name: "Todo List" },
+        { path: "/counter", name: "Counter" },
+        { path: "/area-select", name: "Area-select" },
+        { path: "/currency", name: "Currency" },
+        { path: "/form", name: "Form" },
+        { path: "/style", name: "Style" },
+        { path: "/toggle", name: "Toggle" },
+        { path: "/users", name: "Users" },
+        { path: "/policy", name: "Policy" },
+        { path: "/user-table", name: "UserTalbe" },
+      ];
+    },
+  },
+};
 </script>
 <style>
-  a {
-    margin-right: 1em;
-  }
+a {
+  margin-right: 1em;
+}
 </style>
--- src/ToDoList.vue
+++ src/ToDoList.vue
@@ -1,56 +1,57 @@
 <template>
-    <div>
-        <input v-model="text" ref="ipt">
-        <button @click="add">submit</button>
-        <ul>
-            <li 
-                v-for="item in list" 
-                :key="item.id"
-                @click="toggleDone(item)">
-                <input type="checkbox" v-model="item.done">
-                {{item.text}}
-                <button @click="deleteItem(item)">
-                    delete
-                </button>
-                </li>
-        </ul>
-  </div>
+  <v-container>
+    <v-row class='align-center'>
+      <v-col cols ='8' sm='4'>
+        <v-text-field hide-details outlined v-model="text" ref="ipt" />
+      </v-col>
+      <v-col cols ='4' lg='9'>
+        <v-btn color="primary" @click="add">submit</v-btn>
+      </v-col>
+      <ul>
+        <li v-for="item in list" :key="item.id" @click="toggleDone(item)">
+          <input type="checkbox" v-model="item.done" />
+          {{ item.text }}
+          <v-btn x-small color="error" @click="deleteItem(item)">
+            delete
+          </v-btn>
+        </li>
+      </ul>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data: ()=>({
-        text:'',
-        list:[],
-    }),
-    methods:{
-        deleteItem(item){
-            const idx = this.list.indexOf(item)
-            this.list.splice(idx,1)
-        },
-        toggleDone(item) {
-            item.done = !item.done;
-        },
-        add() {
-            if (this.text==='')
-            {
-                return;
-            }
-            const item = {
-                id: Date.now(),
-                text: this.text,
-                done: false,
-            }
-            this.list.push(item)
-            this.text=''
-        },
+  data: () => ({
+    text: "",
+    list: [],
+  }),
+  methods: {
+    deleteItem(item) {
+      const idx = this.list.indexOf(item);
+      this.list.splice(idx, 1);
     },
-    mounted(){
-        this.$refs.ipt.focus()
-    }
-}
+    toggleDone(item) {
+      item.done = !item.done;
+    },
+    add() {
+      if (this.text === "") {
+        return;
+      }
+      const item = {
+        id: Date.now(),
+        text: this.text,
+        done: false,
+      };
+      this.list.push(item);
+      this.text = "";
+    },
+  },
+  mounted() {
+    this.$refs.ipt.focus();
+  },
+};
 </script>
     
 <style>
-
 </style>
